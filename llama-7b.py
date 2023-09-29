0aaafbb44d93e531bb23e1e35f90d1b454de39ef
Madhu K <madhuk231@gmail.com>
2023-09-29 15:05:24 +0530

    Update llama-7b.py

--- llama-7b.py
+++ llama-7b.py
@@ -3,6 +3,7 @@ import llm
 import time
 from functools import wraps
 from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import BertTokenizer
 
 
 def track_process_time(func):
@@ -23,25 +24,27 @@ def load_preptrained_model(model_path):
 
 @track_process_time
 def load_tokenizer_model(model_path):
-    return AutoTokenizer.from_pretrained(model_path)
+    return AutoTokenizer.from_pretrained('bert-base-uncased')
 
 @track_process_time    
 def generate(input):
     #tokenize input
+    #inputs = [ word.lower() for word in input.split()]
+    #print(inputs)
     model_inputs = tokenizer(
                             input, 
-                            is_pretokenized=True, 
-                            padding=True, 
+                            max_length=100,  
                             truncation=True, 
                             return_tensors="pt")
     print(model_inputs)
     # Generate some text
-    encoded_text = model.generate(**model_inputs, max_length=100)
+    encoded_text = model.generate(input_ids = model_inputs["token_type_ids"], max_length=100)
     return tokenizer.decode(encoded_text)
+    #return encoded_text
 
 
 model_path = "Llama-2-7b-hf"
 model = load_preptrained_model(model_path)
 tokenizer = load_tokenizer_model(model_path)
 
-print(generate(["Once upon on a time"]))
+print(generate("Once upon on a time"))
